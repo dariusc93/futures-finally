d9efa10f7bf2b0ef7b5951cbc46f71295f19ed24
Darius <darius.clark@proton.me>
2024-06-30 15:25:11 -0400

    chore: cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+use futures::{Future, Stream};
+use pin_project::pin_project;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use futures::{Stream, Future};
-use pin_project::pin_project;
 
 #[pin_project]
 pub struct AndFinally<Item, Fut, F, O> {
@@ -10,19 +10,27 @@ pub struct AndFinally<Item, Fut, F, O> {
     #[pin]
     fut: Option<Fut>,
     f: F,
-    output: Option<O>
+    output: Option<O>,
 }
 
 pub trait AndFinallyExt: Sized {
     fn and_finally<Fut: Future, F: Fn() -> Fut, O>(self, f: F) -> AndFinally<Self, Fut, F, O> {
-        AndFinally { item: Some(self), fut: None, f, output: None,}
+        AndFinally {
+            item: Some(self),
+            fut: None,
+            f,
+            output: None,
+        }
     }
 }
 
 impl<T: Sized> AndFinallyExt for T {}
 
-impl<Item: Stream, Fut: Future, F, O> Stream for AndFinally<Item, Fut, F, O> where F: Fn() -> Fut {
-   type Item = Item::Item;
+impl<Item: Stream, Fut: Future, F, O> Stream for AndFinally<Item, Fut, F, O>
+where
+    F: Fn() -> Fut,
+{
+    type Item = Item::Item;
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
 
@@ -32,23 +40,24 @@ impl<Item: Stream, Fut: Future, F, O> Stream for AndFinally<Item, Fut, F, O> whe
                 None => {
                     let fut = Some((this.f)());
                     this.fut.set(fut);
+                    this.item.as_mut().as_pin_mut().take();
                 }
             };
         }
 
-        this.item.as_mut().as_pin_mut().take();
-
         if let Some(fut) = this.fut.as_mut().as_pin_mut() {
             futures::ready!(fut.poll(cx));
+            this.fut.as_mut().as_pin_mut().take();
         }
-
-        this.fut.as_mut().as_pin_mut().take();
 
         Poll::Ready(None)
     }
 }
 
-impl<Item: Future<Output = O>, Fut: Future, F, O> Future for AndFinally<Item, Fut, F, O> where F: Fn() -> Fut, {
+impl<Item: Future<Output = O>, Fut: Future, F, O> Future for AndFinally<Item, Fut, F, O>
+where
+    F: Fn() -> Fut,
+{
     type Output = Item::Output;
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut this = self.project();
@@ -58,15 +67,13 @@ impl<Item: Future<Output = O>, Fut: Future, F, O> Future for AndFinally<Item, Fu
             this.output.replace(output);
             let fut = Some((this.f)());
             this.fut.set(fut);
+            this.item.as_mut().as_pin_mut().take();
         }
-
-        this.item.as_mut().as_pin_mut().take();
 
         if let Some(fut) = this.fut.as_mut().as_pin_mut() {
             futures::ready!(fut.poll(cx));
+            this.fut.as_mut().as_pin_mut().take();
         }
-
-        this.fut.as_mut().as_pin_mut().take();
 
         let output = this.output.take();
 
