dfca9b3e0fac690cfe8f03ea9b2b4e6f1cb11459
Darius <darius.clark@proton.me>
2024-06-30 19:22:41 -0400

    chore use FnOnce instead and add minor test

--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,158 @@
-use futures::{Future, Stream};
-use pin_project::pin_project;
-use std::pin::Pin;
-use std::task::{Context, Poll};
+pub mod future {
+    use pin_project::pin_project;
+    use std::future::Future;
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
 
-#[pin_project]
-pub struct AndFinally<Item, Fut, F, O> {
-    #[pin]
-    item: Option<Item>,
-    #[pin]
-    fut: Option<Fut>,
-    f: F,
-    output: Option<O>,
-}
+    #[pin_project]
+    pub struct ThenFinally<Item, Fut, F, O> {
+        #[pin]
+        item: Option<Item>,
+        #[pin]
+        fut: Option<Fut>,
+        f: Option<F>,
+        output: Option<O>,
+    }
 
-pub trait AndFinallyExt: Sized {
-    fn and_finally<Fut: Future, F: Fn() -> Fut, O>(self, f: F) -> AndFinally<Self, Fut, F, O> {
-        AndFinally {
-            item: Some(self),
-            fut: None,
-            f,
-            output: None,
+    pub trait ThenFinallyFutureExt: Sized {
+        fn then_finally<'a, Fut: Future, F: FnOnce() -> Fut, O>(
+            self,
+            f: F,
+        ) -> ThenFinally<Self, Fut, F, O> {
+            ThenFinally {
+                item: Some(self),
+                fut: None,
+                f: Some(f),
+                output: None,
+            }
+        }
+    }
+
+    impl<T: Sized> ThenFinallyFutureExt for T {}
+
+    impl<Item: Future<Output = O>, Fut: Future, F, O> Future for ThenFinally<Item, Fut, F, O>
+    where
+        F: FnOnce() -> Fut,
+    {
+        type Output = Item::Output;
+        fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            let mut this = self.project();
+
+            if let Some(item) = this.item.as_mut().as_pin_mut() {
+                let output = futures::ready!(item.poll(cx));
+                this.output.replace(output);
+                let func = this.f.take().expect("function is valid");
+                let fut = Some(func());
+                this.fut.set(fut);
+                this.item.as_mut().as_pin_mut().take();
+            }
+
+            if let Some(fut) = this.fut.as_mut().as_pin_mut() {
+                futures::ready!(fut.poll(cx));
+                this.fut.as_mut().as_pin_mut().take();
+            }
+
+            let output = this.output.take();
+
+            Poll::Ready(output.expect("output from future to be value"))
         }
     }
 }
 
-impl<T: Sized> AndFinallyExt for T {}
+pub mod stream {
+    use futures::{Future, Stream};
+    use pin_project::pin_project;
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
 
-impl<Item: Stream, Fut: Future, F, O> Stream for AndFinally<Item, Fut, F, O>
-where
-    F: Fn() -> Fut,
-{
-    type Item = Item::Item;
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let mut this = self.project();
+    #[pin_project]
+    pub struct Finally<Item, Fut, F> {
+        #[pin]
+        item: Option<Item>,
+        #[pin]
+        fut: Option<Fut>,
+        f: Option<F>,
+    }
 
-        if let Some(item) = this.item.as_mut().as_pin_mut() {
-            match futures::ready!(item.poll_next(cx)) {
-                Some(item) => return Poll::Ready(Some(item)),
-                None => {
-                    let fut = Some((this.f)());
-                    this.fut.set(fut);
-                    this.item.as_mut().as_pin_mut().take();
-                }
-            };
+    pub trait FinallyStreamExt: Sized {
+        fn finally<Fut: Future, F: FnOnce() -> Fut>(self, f: F) -> Finally<Self, Fut, F> {
+            Finally {
+                item: Some(self),
+                fut: None,
+                f: Some(f),
+            }
         }
+    }
 
-        if let Some(fut) = this.fut.as_mut().as_pin_mut() {
-            futures::ready!(fut.poll(cx));
-            this.fut.as_mut().as_pin_mut().take();
+    impl<T: Sized> FinallyStreamExt for T {}
+
+    impl<Item: Stream, Fut: Future, F> Stream for Finally<Item, Fut, F>
+    where
+        F: FnOnce() -> Fut,
+    {
+        type Item = Item::Item;
+        fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+            let mut this = self.project();
+
+            if let Some(item) = this.item.as_mut().as_pin_mut() {
+                match futures::ready!(item.poll_next(cx)) {
+                    Some(item) => return Poll::Ready(Some(item)),
+                    None => {
+                        let func = this.f.take().expect("function is valid");
+                        let fut = Some(func());
+                        this.fut.set(fut);
+                        this.item.as_mut().as_pin_mut().take();
+                    }
+                };
+            }
+
+            if let Some(fut) = this.fut.as_mut().as_pin_mut() {
+                futures::ready!(fut.poll(cx));
+                this.fut.as_mut().as_pin_mut().take();
+            }
+
+            Poll::Ready(None)
         }
-
-        Poll::Ready(None)
     }
 }
 
-impl<Item: Future<Output = O>, Fut: Future, F, O> Future for AndFinally<Item, Fut, F, O>
-where
-    F: Fn() -> Fut,
-{
-    type Output = Item::Output;
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut this = self.project();
+#[cfg(test)]
+mod test {
+    use crate::future::ThenFinallyFutureExt;
+    use crate::stream::FinallyStreamExt;
+    use futures::StreamExt;
 
-        if let Some(item) = this.item.as_mut().as_pin_mut() {
-            let output = futures::ready!(item.poll(cx));
-            this.output.replace(output);
-            let fut = Some((this.f)());
-            this.fut.set(fut);
-            this.item.as_mut().as_pin_mut().take();
-        }
+    #[test]
+    fn future_final() {
+        futures::executor::block_on(async move {
+            let mut val = 0;
 
-        if let Some(fut) = this.fut.as_mut().as_pin_mut() {
-            futures::ready!(fut.poll(cx));
-            this.fut.as_mut().as_pin_mut().take();
-        }
+            futures::future::ready(())
+                .then_finally(|| async {
+                    val = 1;
+                })
+                .await;
 
-        let output = this.output.take();
+            assert_eq!(val, 1);
+        });
+    }
 
-        Poll::Ready(output.expect("output from future to be value"))
+    #[test]
+    fn stream_final() {
+        futures::executor::block_on(async move {
+            let mut val = 0;
+
+            let st = futures::stream::once(async { 0 }).finally(|| async {
+                val = 1;
+            });
+
+            futures::pin_mut!(st);
+
+            while let Some(v) = st.next().await {
+                assert_eq!(v, 0);
+            }
+
+            assert_eq!(val, 1);
+        });
     }
 }
